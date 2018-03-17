103069c3b91f18949274817d7e0363a0ebad8475
aagam <aagam@localhost.localdomain>
2018-03-17 00:24:27 -0700

    implement token sending function

--- app.js
+++ app.js
@@ -8,41 +8,12 @@ const civicSip = require('civic-sip-api');
 const PRIVATE_KEY = "c6d9c62b87efb22c2acea601b213cf9255716042e0a05c9b10b9ef41d7c5a97b"
 const APP_SECRET = "ae44d7abe12309b01e611045b08a55f3"
 
-const jwtToken = {
-  header: {
-    typ: 'JWT', alg: 'ES256'
-  },
-  payload: {
-    jti: '45a59d10-6e93-47f6-9185-adacfe28907a',
-    iat: 1494204971.361,
-    exp: 1494205151.361,   // 3 minute lifespan
-    iss: 'civic-sip-hosted-service',
-    aud: '/dev/scopeRequest/authCode',  // valid endpoints for this token
-    sub: 'civic-sip-hosted-service',
-    data: {
-      codeToken: '81f2564c-e7c0-4869-be49-a88f5738534f'
-    }
-  },
-  signature: 'xC3CYCFz-p0RWf2CLnnvfb7Fhksu9vSJgHKQIsP4iakp4HI63xoWZZqEiDgcV5S7CULIty_v0fpvJwSs87BgWQ'
-}
-
 // Step 4: Initialize instance passing your appId and secret.
 const civicClient = civicSip.newClient({
   appId: 'HJcbwZcYG',
   prvKey: PRIVATE_KEY,
   appSecret: APP_SECRET,
 });
-
-
-// Step 5: Exchange authorization code for user data.
-civicClient.exchangeCode(jwtToken)
-    .then((userData) => {
-        // store user data and userId as appropriate
-        console.log('userData = ', JSON.stringify(userData, null, 4));
-    }).catch((error) => {
-        console.log(error);
-    });
-
 
 app.use(express.static(__dirname+'/client'));
 app.use(bodyParser.json());
@@ -104,6 +75,19 @@ app.delete('/api/games/:_id', function(req, res){
 		res.json(game);
 	});
 });
+
+app.post('/api/civic'), function(req, res){
+	var jwtToken = req.body;
+
+	// Step 5: Exchange authorization code for user data.
+	civicClient.exchangeCode(jwtToken)
+    .then((userData) => {
+        // store user data and userId as appropriate
+        console.log('userData = ', JSON.stringify(userData, null, 4));
+    }).catch((error) => {
+        console.log(error);
+    });
+}
 
 app.listen(app.get('port'), function(){
 	console.log('Running on port ', app.get('port'));
--- client/controllers/games.js
+++ client/controllers/games.js
@@ -3,6 +3,38 @@ var myApp = angular.module('myApp');
 myApp.controller('GamesController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
 	console.log('GamesController loaded...');
 
+	var civicSip = new civic.sip({ appId: 'HJcbwZcYG' });
+	var button = document.querySelector('#signupButton');
+
+	button.addEventListener('click', function () {
+		civicSip.signup({ style: 'popup', scopeRequest: civicSip.ScopeRequests.BASIC_SIGNUP });
+	});
+
+	// Listen for data
+	civicSip.on('auth-code-received', function (event) {
+
+		// encoded JWT Token is sent to the server
+		var jwtToken = event.response;
+		http.post('/api/civic', jwtToken).success(function(response){
+					console.log(jwtToken);
+		});
+	});
+
+	civicSip.on('user-cancelled', function (event) {
+		console.log("User cancelled");
+	});
+
+	civicSip.on('read', function (event) {
+		console.log("Read");
+	});
+
+	// Error events.
+	civicSip.on('civic-sip-error', function (error) {
+	  // handle error display if necessary.
+	  console.log('   Error type = ' + error.type);
+	  console.log('   Error message = ' + error.message);
+	});
+
 	$scope.getGames = function(){
 		$http.get('/api/games').success(function(response){
 			$scope.games = response;
